a1b92982ad3653c2b1549f801108f7bf763ce1cd
hata-tomoyuki <h.tomo0602@gmail.com>
2022-07-18 20:10:52 +0900

    add logic for register function

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,21 @@
+import { useContext, useState } from "react";
 import { Button, Col, Container, Form, FormGroup, Input, Label, Row } from "reactstrap";
+import AppContext from "../context/AppContext";
+import { registerUser } from "../lib/auth";
 
 const register = () => {
+    
+    const appContext = useContext(AppContext);
+
+    const [data, setData] = useState({ username: "", email: "", password: "" });
+    const handleRegister = () => {
+        registerUser(data.username, data.email, data.password)
+        .then((res) => {
+            appContext.setUser(res.data.user);
+        })
+        .catch((err) => console.log(err));
+    };
+
     return (
         <Container>
             <Row>
@@ -15,22 +30,43 @@ const register = () => {
                             <fieldset>
                                 <FormGroup>
                                     <Label>ユーザー名：</Label>
-                                    <Input type="text" name="username" style={{height: 50, fontSize: "1.2rem"}} />
+                                    <Input 
+                                        type="text" 
+                                        name="username" 
+                                        style={{height: 50, fontSize: "1.2rem"}} 
+                                        onChange={(e) => setData({ ...data, username: e.target.value })}
+                                    />
                                 </FormGroup>
                                 <FormGroup>
                                     <Label>メールアドレス：</Label>
-                                    <Input type="email" name="email" style={{height: 50, fontSize: "1.2rem"}} />
+                                    <Input 
+                                        type="email" 
+                                        name="email" 
+                                        style={{height: 50, fontSize: "1.2rem"}} 
+                                        onChange={(e) => setData({ ...data, email: e.target.value })}
+                                    />
                                 </FormGroup>
                                 <FormGroup>
                                     <Label>パスワード：</Label>
-                                    <Input type="password" name="password" style={{height: 50, fontSize: "1.2rem"}} />
+                                    <Input 
+                                        type="password" 
+                                        name="password" 
+                                        style={{height: 50, fontSize: "1.2rem"}} 
+                                        onChange={(e) => setData({ ...data, password: e.target.value })}
+                                    />
                                 </FormGroup>
                                 <span>
                                     <a href="">
                                         <small>パスワードをお忘れですか？</small>
                                     </a>
                                 </span>
-                                <Button style={{ float: "right", width: 120, marginTop: 50 }} color="primary ">
+                                <Button 
+                                    style={{ float: "right", width: 120, marginTop: 50 }} 
+                                    color="primary"
+                                    onClick={() => {
+                                        handleRegister();
+                                    }}
+                                >
                                     登録
                                 </Button>
                             </fieldset>
