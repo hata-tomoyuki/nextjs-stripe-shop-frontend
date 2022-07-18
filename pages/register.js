8a5c8811b46020d9c23e91dd465103265a325eb8
hata-tomoyuki <h.tomo0602@gmail.com>
2022-07-18 20:21:25 +0900

    add header change logic

--- pages/register.js
+++ pages/register.js
@@ -10,8 +10,8 @@ const register = () => {
     const [data, setData] = useState({ username: "", email: "", password: "" });
     const handleRegister = () => {
         registerUser(data.username, data.email, data.password)
-        .then((res) => {
-            appContext.setUser(res.data.user);
+        .then(() => {
+            appContext.setUser({ ...data });
         })
         .catch((err) => console.log(err));
     };
